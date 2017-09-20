f27d3f7945e39bdee5d15edb11059430e880b6f5
Carlos R. Villanueva Ousset <crvo84@gmail.com>
2017-09-20 01:30:34 -0500

    Initial merge sort implementation.

--- Sources/App/SortBrain.swift
+++ Sources/App/SortBrain.swift
@@ -13,10 +13,26 @@ enum SortAlgorithm {
 }
 
 struct SortBrain {
-    static func sort<U: Comparable>(type: U.Type, array: [U], algorithm: SortAlgorithm) -> [U] {
+    static func sort(array: [Int], algorithm: SortAlgorithm) -> [Int] {
         
         // TODO: implement algorithm manually
         let sortedArray = array.sorted()
         return sortedArray
     }
+    
+    private func mergeSort(array: [Int], leftIndex: Int, rightIndex: Int) -> [Int] {
+        // if the number of elements is <= 1, return same array
+        guard leftIndex < rightIndex else { return array }
+        
+        let middleIndex = (leftIndex + rightIndex) / 2
+        
+        let leftHalf =
+        let rightHalf = array[middleIndex+1...rightIndex]
+        
+        let sortedLeftHalf = mergeSort(array: array[0...middleIndex], leftIndex: <#T##Int#>, rightIndex: <#T##Int#>)
+        
+        return []
+    }
+    
+    
 }
